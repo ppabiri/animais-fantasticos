08d082083c1bea32a799c4bf6fb6ba5ff36b22af
ppabiri <125708346+ppabiri@users.noreply.github.com>
2025-11-12 10:12:21 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+const h1 = document.querySelector('h1');
+const animaisLista = document.querySelector('.animais-lista');
+h1.innerHTML; // retorna "Animais Fantásticos"
+animaisLista.innerHTML; // retorna todos os <li> 
